3cce041775eee16bce8853cea5e04f0021a2071b
Econa77 <s.f.1992.ip@gmail.com>
2015-07-29 00:20:11 +0900

    Add preference viewcontrollers

---
+++ Clipy/WindowControllers/PreferenceWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  PreferenceWindowController.swift
+//  Clipy
+//
+//  Created by 古林俊佑 on 2015/07/29.
+//  Copyright (c) 2015年 Shunsuke Furubayashi. All rights reserved.
+//
+
+import Cocoa
+
+class PreferenceWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
